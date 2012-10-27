49a1786c481c909307e85656d2c88d628e8c4a6a
ChanMin Kim <kcm1700@gmail.com>
2012-10-28 03:30:45 +0900

    Add C++ template countof, byte reverser, etc.
    - Simple arithmetic (factorial, power)
    - countof
    - byte reverser

---
+++ compile_constants.cpp
@@ -0,0 +1,79 @@
+#include <stddef.h>
+#include <stdint.h>
+
+template<int a, int b> struct tmin{enum{v = a<b?a:b};};
+template<int a, int b> struct tmax{enum{v = a>b?a:b};};
+
+template<int a> struct tfactorial{enum{v = a * tfactorial<a-1>::v};};
+template<> struct tfactorial<0>{enum{v = 1};};
+template<uint32_t a, uint32_t mod> struct tfactorialmod{enum{v = (uint32_t)((uint64_t)a * (uint64_t)tfactorial<a-1>::v % mod)};};
+template<uint32_t mod> struct tfactorialmod<0,mod>{enum{v = 1u};};
+
+template<int a, int b> struct tpow{enum{v = tpow<a,b/2>::v * tpow<a,b-b/2>::v};};
+template<int a> struct tpow<a, 1>{enum{v = a};};
+template<int a> struct tpow<a, 0>{enum{v = 1};};
+
+template<uint32_t a, uint32_t b, uint32_t mod> struct tpowmod{enum{v = (uint32_t)((uint64_t)tpowmod<a,b/2,mod>::v * (uint64_t)tpowmod<a,b-b/2,mod>::v % mod)};};
+template<uint32_t a, uint32_t mod> struct tpowmod<a, 1, mod>{enum{v = a%mod};};
+template<uint32_t a, uint32_t mod> struct tpowmod<a, 0, mod>{enum{v = 1u};};
+
+/* countof
+ * Usage: countof(some_array) */
+#ifndef countof
+template<typename T, size_t n> char (&countof_helper(T(&)[n]))[n];
+#define countof(array) sizeof(countof_helper(array))
+#endif
+
+/* Endianness
+ * TReverseByteOrder: reverse the byte representation of integer
+ * TReverseBitOrder: reverse the bit representation of integer
+ * usage:
+ * to get reverse byte order of 12345678u, TReverseByteOrder<uint32_t, 12345678u{{, 4}}>::v
+ * to get reverse bit order of 12345678u, TReverseBitOrder<uint32_t, 12345678u{{, 32}}>::v
+ * */
+template<typename T, T n, size_t bytes = sizeof(T)> class TReverseByteOrder {
+	template<size_t size, T m> struct reverser {
+		enum {
+			lowBytes = size/2,
+			highBytes = size-lowBytes,
+			v = (reverser<lowBytes, m&(((T)1<<lowBytes*8)-(T)1)>::v << 8*highBytes)
+			| reverser<highBytes, m>>8*lowBytes>::v
+		};
+	};
+	template<T m> struct reverser<1,m>{enum{v = m };};
+public:
+	enum{v = reverser<bytes, n>::v};
+};
+
+template<typename T, T n, size_t bits = 8*sizeof(T)> class TReverseBitOrder {
+	template<size_t size, T m> struct reverser {
+		enum {
+			lowBits = size/2,
+			highBits = size-lowBits,
+			v = (reverser<lowBits, m&(((T)1<<lowBits)-(T)1)>::v << highBits)
+			| reverser<highBits, m>>lowBits>::v
+		};
+	};
+	template<T m> struct reverser<1,m>{enum{v = m };};
+public:
+	enum{v = reverser<bits, n>::v};
+};
+
+// test code
+#include <stdio.h>
+
+template<int line> void my_assert_inner(bool result)
+{
+	if (result) return;
+	printf("test failed at line %d!\n", line);
+}
+
+#define my_assert my_assert_inner<__LINE__>
+
+int main()
+{
+	my_assert(tmin<1700,1500>::v == 1500);
+	my_assert(countof("asdf") == 5);
+	my_assert(TReverseByteOrder<uint32_t,15838294u>::v == 1454174464u);
+	return 0;
+}

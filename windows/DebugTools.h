6c06bdf395d5b429ba34b1f69ceeeff8a464a2cd
ChanMin Kim <kcm1700@gmail.com>
2013-01-28 18:04:43 +0900

    VS의 format string 경고가 DebugString을 쓸 때도 뜨도록 변경

--- windows/DebugTools.h
+++ windows/DebugTools.h
@@ -1,8 +1,15 @@
 #pragma once
 
+#if _MSC_VER >= 1500
+#include <stdio.h>
+#define DebugStringConstraint _In_z_ _Printf_format_string_
+#else
+#define DebugStringConstraint
+#endif
+
 namespace DebugTools
 {
-    void __stdcall DebugString(const char *pFormat, ...);
+    void __stdcall DebugString(DebugStringConstraint const char *pFormat, ...);
     void __stdcall SetThreadName( DWORD dwThreadID, char* threadName);
 }
 
